cba97ca31e74d13a02272fa34893a57e18929bcb
George Koreev <georgekoreev@mail.ru>
2019-05-09 13:02:16 +0300

    Добавлен тест на редактирование группы

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,17 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # choose edit group
+        wd.find_element_by_name("edit").click()
+        # submit editing
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
