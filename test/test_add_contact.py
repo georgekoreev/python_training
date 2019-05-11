fa628c2d49cfc9752dbb0d014a0df3dcc72f38ab
George Koreev <georgekoreev@mail.ru>
2019-05-11 17:39:21 +0300

    Задание №8

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    #app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(name="Ivan", sname="Ivanov", address="Moscow"))
-    #app.session.logout()
 
 def test_add_empty_contact(app):
-    #app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(name="", sname="", address=""))
-    #app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,7 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
-    #app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(name="New contact"))
-    #app.session.logout()
-
 
 def test_modify_sname(app):
-    #app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(sname="New sname"))
-    #app.session.logout()
