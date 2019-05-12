3a328170ae6bd0ac267f88f05e99b40d7a3167ab
George Koreev <georgekoreev@mail.ru>
2019-05-12 09:54:53 +0300

    Задание №9. Обеспечение выполнения предусловий для групп и контактов

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,11 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(name="test"))
     app.contact.modify_first_contact(Contact(name="New contact"))
 
 def test_modify_sname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(name="test"))
     app.contact.modify_first_contact(Contact(sname="New sname"))
