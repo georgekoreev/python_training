922de2e90aa94e11ac222eb5c7c7f98fb9979ccd
George Koreev <georgekoreev@mail.ru>
2019-05-29 22:42:20 +0300

    15 задание для контактов

--- model/contact.py
+++ model/contact.py
@@ -23,7 +23,9 @@ class Contact:
         self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % (self.id, self.firstname, self.company, self.address, self.home,
+                                                        self.work, self.mobile, self.phone2, self.email, self.email2,
+                                                        self.email3, self.address2)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,28 +1,44 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="",
+                lastname="",
+                company="",
+                address="",
+                home="",
+                work="",
+                mobile="",
+                phone2="",
+                email="",
+                email2="",
+                email3="",
+                address2="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), company=random_string("company", 15),
+         address=random_string("address", 20), home=random_string("hometel", 20), work=random_string("worktel", 20),
+         mobile=random_string("mobiletel", 20), phone2=random_string("phone2tel", 20), email=random_string("email@", 10),
+         email2=random_string("email2@", 10), email3=random_string("email3@", 10), address2=random_string("address2", 20),)
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", lastname="Ivanov", address="Moscow",
-                      company="Google", home="123454", work="5464722",
-                      mobile="4672392", phone2="676273",
-                      email="kotiki@mail.ru", address2="Magadan")
     app.contact.create_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == app.contact.count()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-#def test_add_empty_contact(app):
-#    old_contacts = app.contact.get_contact_list()
-#    contact = Contact(name="", sname="", address="")
-#    app.contact.create_contact(contact)
-#    new_contacts = app.contact.get_contact_list()
-#    assert len(old_contacts) + 1 == len(new_contacts)
-#    old_contacts.append(contact)
-#    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 
